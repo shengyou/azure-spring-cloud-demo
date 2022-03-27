cea5cf21f098c799642b5cc6115c8e8e5ea7d625
Shengyou Fan <shengyoufan@gmail.com>
2022-03-28 01:27:45 +0800

    add javafaker dependency

--- product-service/build.gradle.kts
+++ product-service/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.javafaker:javafaker:1.0.2")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
