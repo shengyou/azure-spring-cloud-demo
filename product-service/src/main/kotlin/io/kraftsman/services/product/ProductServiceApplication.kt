a83477e54b0afb2e2ebc6760c2e47d8d4520f59a
Shengyou Fan <shengyoufan@gmail.com>
2022-03-28 00:30:28 +0800

    add product service

---
+++ product-service/src/main/kotlin/io/kraftsman/services/product/ProductServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.kraftsman.services.product
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductServiceApplication>(*args)
+}
---
+++ product-service/src/test/kotlin/io/kraftsman/services/product/ProductServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.kraftsman.services.product
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
