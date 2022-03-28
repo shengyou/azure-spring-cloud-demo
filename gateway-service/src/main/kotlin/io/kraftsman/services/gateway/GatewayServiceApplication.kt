b94d2ab1d8ed5f6200ca991721b662b7091a30d6
Shengyou Fan <shengyoufan@gmail.com>
2022-03-28 23:04:45 +0800

    add gateway service related code

---
+++ gateway-service/src/main/kotlin/io/kraftsman/services/gateway/GatewayServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.kraftsman.services.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayServiceApplication>(*args)
+}
